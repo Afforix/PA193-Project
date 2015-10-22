7c60818b46d54749f799644463c7f36daaed27a2
Afforix <plch.matej@gmail.com>
2015-10-22 19:04:13 +0200

    token 2 - fixing previous commit

--- token.h
+++ token.h
@@ -1,9 +1,41 @@
-enum class token_type { _types }
+#include <string>
 
-class token 
+enum token_type { _types }; // TODO
+
+class token
 {
-    token_type type; // bracket, colon...
-    std::string text; // text of string
-    
-}
+    token_type _type; // bracket, string...
+    std::string _text; // text of string
+
+public:
+
+    /**
+     * @brief token ctor for non-strings
+     * @param type_
+     */
+    token(token_type type_)
+        : _type(type_)
+    {}
+
+    /**
+     * @brief token ctor for strings
+     * @param type_
+     * @param text_
+     */
+    token(token_type type_, const std::string& text_)
+        : _type(type_), _text(text_)
+    {}
+
+    /**
+     * @brief type
+     * @return token type
+     */
+    token_type type() const { return _type; }
+
+    /**
+     * @brief text
+     * @return text of string token
+     */
+    const std::string& text() const { return _text; }
+};
 
