52e7188fd947b4cc5ddcbd49a70da3992e42cca9
zidhub <milan@defendio.net>
2015-10-27 14:08:42 +0100

    Added basic for syntax parsing

--- parser.cpp
+++ parser.cpp
@@ -31,14 +31,42 @@ int main(int argc, char const* argv[])
         return 1;
     }
 
+    int err = 0;
+
     while (true)
     {
          token tok = json.get_token();
          tok.print();
-         if (tok.type() == T_EOF || tok.type() == T_ERR)
+         
+         if (tok.type() == T_EOF) {
+             // we are out of tokens, but tree is not finished
+             if (!root.is_finished()) {
+                 err = 1;
+             }
              break;
+         } else if (tok.type() == T_ERR) {
+             err = 2;
+             break;
+         }
 
+         // can't add token to the tree
+         if (!root.add(tok)) {
+             err = 1;
+             break;
+         }
     }
+
+    // if there are any problems, write them
+    if (err == 0) {
+        // XXX ok, we can continue to semantic analysis
+    } else if (err == 1) {
+        std::cerr << "Syntax is incorrect" << endl;
+    } else if (err == 2) {
+        std::cerr << "Lexems are incorrect" << endl;
+    } else {
+        std::cerr << "Some unexpected error occured" << endl;
+    }
+
 
     return 0;
 }
