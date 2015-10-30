4ff7f2eb30b97f699ce42dc2eba247ce5cd2f366
Peter Kotvan <peter.kotvan@gmail.com>
2015-10-30 09:38:12 -0400

    Minor tweaks in test script.

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-echo "All of the following tests should fail with bad lexemes msg:"
+echo "All of the following tests should fail."
 for i in fail* ; do ../parser $i ;  done 1> /dev/null
+echo '=== EOF ==='
 
 echo
 echo "Following test runs ../parser pass.j and compares the output using diff."
