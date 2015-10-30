c103c387670046f0943a9df2ada6a59c25f41cb8
Peter Kotvan <peter.kotvan@gmail.com>
2015-10-30 16:52:08 -0400

    Rename test files *.j -> *.json.

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "All of the following tests should fail."
-for i in fail* ; do ../parser $i ;  done 1> /dev/null
+for i in fail*.json ; do ../parser $i ;  done 1> /dev/null
 echo '=== EOF ==='
 
 echo
 echo "Following test runs ../parser pass.j and compares the output using diff."
 echo "In the left column there is a reference and in the right is the actual output."
-../parser pass.j > pass.out
+../parser pass.json > pass.out
 
 echo
 diff -y --suppress-common-lines pass.ref pass.out
