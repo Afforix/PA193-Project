e614c280a886004dbd7fa531aef7abd66f00a9a3
zidhub <milan@defendio.net>
2015-10-31 21:25:52 +0100

    Semantic analysis of config
    	modified:   analysis.cpp

--- analysis.cpp
+++ analysis.cpp
@@ -51,12 +51,12 @@ is_iso8601_datetime (std::string s)
 bool
 is_user_group (std::string s)
 {
-  std::regex uid ("^[0-9]+$");
-  std::regex user ("^[a-z_][a-z0-9_-]*[$]?$");
-  std::regex user_group ("^[a-z_][a-z0-9_-]*[$]?:[0-9a-zA-Z]+$");
-  std::regex uid_guid ("^[0-9]+:[0-9]+$");
-  std::regex uid_group ("^[0-9]+:[a-z_][a-z0-9_-]*[$]?$");
-  std::regex user_gid ("^[a-z_][a-z0-9_-]*[$]?:[0-9]+$");
+  std::regex uid ("^\"[0-9]+\"$");
+  std::regex user ("^\"[a-zA-Z_][a-zA-Z0-9_-]*\"$");
+  std::regex user_group ("^\"[a-zA-Z_][a-zA-Z0-9_-]*:[a-zA-Z_][a-zA-Z0-9_-]*\"$");
+  std::regex uid_guid ("^\"[0-9]+:[0-9]+\"$");
+  std::regex uid_group ("^\"[0-9]+:[a-zA-Z_][a-zA-Z0-9_-]*\"$");
+  std::regex user_gid ("^\"[a-zA-Z_][a-zA-Z0-9_-]*:[0-9]+\"$");
 
   if (std::regex_match (s, uid)) {
       return true;
@@ -88,9 +88,9 @@ is_user_group (std::string s)
 bool
 is_port_spec (std::string s)
 {
-    std::regex port ("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
-    std::regex udp ("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])/udp$");
-    std::regex tcp ("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])/tcp$");
+    std::regex port ("^\"([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])\"$");
+    std::regex udp ("^\"([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])/udp\"$");
+    std::regex tcp ("^\"([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])/tcp\"$");
 
     if (std::regex_match (s, port)) {
         return true;
@@ -110,7 +110,7 @@ is_port_spec (std::string s)
 bool
 is_env_spec (std::string s)
 {
-    std::regex env ("^[A-Z]+=[^=]*$");
+    std::regex env ("\"^[A-Z]+=[^=]*$\"");
 
     if (std::regex_match (s, env)) {
         return true;
@@ -134,45 +134,145 @@ is_integer // XXX probably not necessary; change syntax analysis?
 
 */
 
+bool
+is_valid_env (std::shared_ptr< json_value > val_)
+{
+    return true;
+}
+
+bool
+is_valid_exposedports (std::shared_ptr< json_value > val_)
+{
+    return true;
+}
+
 
 bool
 is_valid_config (std::shared_ptr< json_value > val_)
 {
     bool ret = true;
 
-/*
- // XXX nothing is mandatory!
-    User -> J_STRING, is_user_group()
-    Memory -> J_INT
-    MemorySwap -> J_INT
-    CpuShares -> J_INT
-    ExposedPorts -> J_OBJECT, is_valid_exposedports()
-    Env -> J_ARRAY, is_valid_env()
-    Entrypoint, J_ARRAY
-    Cmd, J_ARRAY
-    Volumes, J_OBJECT
-    WorkingDir, J_OBJECT
-*/
+    /*
+    * User -> J_STRING, is_user_group()
+    * Memory -> J_INT
+    * MemorySwap -> J_INT
+    * CpuShares -> J_INT
+    * ExposedPorts -> J_OBJECT, is_valid_exposedports()
+    * Env -> J_ARRAY, is_valid_env()
+    * Entrypoint, J_ARRAY
+    * Cmd, J_ARRAY
+    * Volumes, J_OBJECT
+    * WorkingDir, J_OBJECT
+    */
 
-    #define CONFIG_ITEMS_LENGTH 10
-    std::string config_items[] = {"\"User\"", "\"Memory\"", "\"MemorySwap\"",
-    "\"CpuShares\"", "\"ExposedPorts\"", "\"Env\"", "\"Entrypoint\"",
-    "\"Cmd\"", "\"Volumes\"", "\"WorkingDir\""};
+    auto object = std::static_pointer_cast< json_object >(val_);
 
-    if (val_->jtype() == json_type::J_OBJECT) {
-        for (int i = 0; i < CONFIG_ITEMS_LENGTH; i++) {
-            if (!std::static_pointer_cast <json_object>(val_)->contains (config_items[i])) {
-                std::cerr << "config doesn't contain " << config_items[i] << std::endl;
+    // validate Env
+    auto Env = object->find("\"Env\"");
+    if (Env != nullptr) {
+        if (Env->jtype() != json_type::J_ARRAY) {
+            std::cerr << "Env isn't array" << std::endl;
+            ret = false;
+        } else {
+            if (!is_valid_env(Env)) {
+                std::cerr << "Env isn't valid env array" << std::endl;
                 ret = false;
             }
         }
-    } else {
-        ret = false;
+    }
+
+    // validate ExposedPorts
+    auto ExposedPorts = object->find("\"ExposedPorts\"");
+    if (ExposedPorts != nullptr) {
+        if (ExposedPorts->jtype() != json_type::J_OBJECT) {
+            std::cerr << "ExposedPorts isn't object" << std::endl;
+            ret = false;
+        } else {
+            if (!is_valid_exposedports(ExposedPorts)) {
+                std::cerr << "ExposedPorts isn't valid exposedports object" << std::endl;
+                ret = false;
+            }
+        }
+    }
+
+    // validate WorkingDir
+    auto WorkingDir = object->find("\"WorkingDir\"");
+    if (WorkingDir != nullptr) {
+        if (WorkingDir->jtype() != json_type::J_STRING) {
+            std::cerr << "WorkingDir isn't object" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate Volumes
+    auto Volumes = object->find("\"Volumes\"");
+    if (Volumes != nullptr) {
+        if (Volumes->jtype() != json_type::J_OBJECT) {
+            std::cerr << "Volumes isn't object" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate Cmd
+    auto Cmd = object->find("\"Cmd\"");
+    if (Cmd != nullptr) {
+        if (Cmd->jtype() != json_type::J_ARRAY) {
+            std::cerr << "Cmd isn't array" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate Entrypoint
+    auto Entrypoint = object->find("\"Entrypoint\"");
+    if (Entrypoint != nullptr) {
+        if (Entrypoint->jtype() != json_type::J_ARRAY) {
+            std::cerr << "Entrypoint isn't array" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate CpuShares
+    auto CpuShares = object->find("\"CpuShares\"");
+    if (CpuShares != nullptr) {
+        if (CpuShares->jtype() != json_type::J_INT) {
+            std::cerr << "CpuShares isn't int" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate MemorySwap
+    auto MemorySwap = object->find("\"MemorySwap\"");
+    if (MemorySwap != nullptr) {
+        if (MemorySwap->jtype() != json_type::J_INT) {
+            std::cerr << "MemorySwap isn't int" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate Memory
+    auto Memory = object->find("\"Memory\"");
+    if (Memory != nullptr) {
+        if (Memory->jtype() != json_type::J_INT) {
+            std::cerr << "Memory isn't int" << std::endl;
+            ret = false;
+        }
+    }
+
+    // validate User
+    auto User = object->find("\"User\"");
+    if (User != nullptr) {
+        if (User->jtype() != json_type::J_STRING) {
+            std::cerr << "User isn't string" << std::endl;
+            ret = false;
+        } else {
+            if (!is_user_group(User->to_string())) {
+                std::cerr << "User isn't valid user_group string" << std::endl;
+                ret = false;
+            }
+        }
     }
 
     return ret;
-
-
 }
 
 
@@ -340,7 +440,5 @@ do_semantic_analysis (std::shared_ptr<json_value> val_)
         ret = false;
     }
 
-
     return ret;
 }
-
